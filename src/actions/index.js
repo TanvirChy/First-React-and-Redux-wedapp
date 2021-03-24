39db9b815e0c8d125565e28130d4e49d0cb79a40
tanvir <tanvir@pop-os.localdomain>
2021-03-24 22:56:28 +0600

    first commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 
-export const selectSong= song =>{
-    return{
-        type:'SONG_SELECTED',
-        payload: song
-    };
+export const fetchPosts = ()=>  async dispatch=>{
+    const response = await jsonPlaceholder.get('/posts');
+    dispatch({type:'FETCH_POST',payload:response.data});
 };
+
+
